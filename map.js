b611e05dbc12b745827e463ae2fb876317cce540
James Klassen <jameswklassen@gmail.com>
2021-03-18 13:36:06 -0500

    add markers

--- map.js
+++ map.js
@@ -1,10 +1,15 @@
 const lat = 49.892665;
 const lon = -97.143768;
+
+const start = [-97.143914, 49.892207]
+const dest = [-97.14385,  49.893019]
+
+const center = [lon, lat]
 mapboxgl.accessToken = 'pk.eyJ1IjoiamFtZXNrbGFzc2VuIiwiYSI6ImNrbWYzOW0xdTMwZHAycHcwMXVvNDR0bjgifQ.AdOE2Tt6HU-_Wqn-NLvRzw';
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [lon, lat],
+    center,
     zoom: 17
 });
 
@@ -17,10 +22,11 @@ map.on('load', function () {
             'geometry': {
                 'type': 'LineString',
                 'coordinates': [
-                    [-97.143914, 49.892207],
+                    start,
+                    // [-97.143914, 49.892207],
                     [-97.144306, 49.892878],
                     [-97.144091, 49.892974],
-                    [-97.14385,  49.893019],
+                    dest,
                 ]
             }
         }
@@ -38,4 +44,16 @@ map.on('load', function () {
             'line-width': 8
         }
     });
+
+    // start marker
+    new mapboxgl.Marker({
+        color: "#469bd3",
+    }).setLngLat(start)
+    .addTo(map);
+
+    // destination marker
+    new mapboxgl.Marker({
+        color: "#469bd3",
+    }).setLngLat(dest)
+    .addTo(map);
 });
