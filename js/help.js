967e13422ad860acaa1092798e21f01b85e03b90
James Klassen <jameswklassen@gmail.com>
2021-03-19 19:38:54 -0500

    Add help button to navigation pages

---
+++ js/help.js
@@ -0,0 +1,20 @@
+var overlay = document.getElementById("modal-overlay");
+var btn = document.getElementById("help-button");
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = () => {
+  overlay.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = () => {
+  overlay.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = (event) => {
+  if (event.target == overlay) {
+    overlay.style.display = "none";
+  }
+}
